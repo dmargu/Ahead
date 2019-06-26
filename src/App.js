3b06b406e8c830017e99930653058de8dcaf59fa
dmargu <danielmargulies@rocketmail.com>
2019-06-26 16:14:46 -0500

    showing jon

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing out making a change</Text>
+      <Text>making a chang</Text>
     </View>
   );
 }
