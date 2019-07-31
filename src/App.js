eafdf1ba972754cc27d67f72dd18a917614c5d56
dmargu <danielmargulies@rocketmail.com>
2019-07-31 13:44:18 -0400

    deleted unused global items array and unused dependencies from project.

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ export default class App extends Component {
       },
     }));
     /* eslint-disable no-underscore-dangle */
+    /* eslint-disable no-undef */
     const store = createStore(reducers, {},
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
     /* eslint-enable */
